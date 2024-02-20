300e83a04e891bea3544c90c615e76976d28f1d4
danilsyah <danilsyaharihardjo@gmail.com>
2024-02-20 10:33:50 +0700

    materi pertama terkait intro golang dan installasi golang

---
+++ 01_helloworld/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		komentar kode
+		menghasilkan pesan hello world
+	*/
+
+	// result Hello World
+	fmt.Println("Hello World")
+	// Danil Syah
+	fmt.Println("Danil", "Syah")
+
+	// Airell jornal
+	fmt.Print("Airell jornal\n")
+	// ucup kujang
+	fmt.Print("ucup kujang\n")
+	// danilsyah
+	fmt.Print("danil", "syah")
+
+}
