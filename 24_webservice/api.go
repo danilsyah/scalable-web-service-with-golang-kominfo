3df1884ff4b38be411d8b168b224cc731c1c7099
danilsyah <danilsyaharihardjo@gmail.com>
2024-03-03 03:27:46 +0700

    24 materi webservice api

---
+++ 24_webservice/api.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+type Employee struct {
+	ID       int
+	Name     string
+	Age      int
+	Division string
+}
+
+var employees = []Employee{
+	{ID: 1, Name: "Danil", Age: 29, Division: "IT"},
+	{ID: 2, Name: "Nufika", Age: 28, Division: "Marketing"},
+	{ID: 3, Name: "Haykal", Age: 5, Division: "Owner"},
+}
+
+var PORT = ":8080"
+
+func main() {
+	http.HandleFunc("/employees", getEmployees)
+	http.HandleFunc("/employee", createEmployee)
+	http.HandleFunc("/employeesHtml", getEmployeesHtml)
+
+	fmt.Println("Application is listening on port", PORT)
+	http.ListenAndServe(PORT, nil)
+}
+
+func getEmployees(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == "GET" {
+		json.NewEncoder(w).Encode(employees)
+		return
+	}
+
+	http.Error(w, "Invalid method", http.StatusBadRequest)
+}
+
+func createEmployee(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		age := r.FormValue("age")
+		division := r.FormValue("division")
+
+		convertAge, err := strconv.Atoi(age)
+
+		if err != nil {
+			http.Error(w, "Invalid Age", http.StatusBadRequest)
+			return
+		}
+
+		newEmployee := Employee{
+			ID:       len(employees) + 1,
+			Name:     name,
+			Age:      convertAge,
+			Division: division,
+		}
+
+		employees = append(employees, newEmployee)
+
+		json.NewEncoder(w).Encode(newEmployee)
+		return
+	}
+
+	http.Error(w, "Invalid method", http.StatusBadRequest)
+}
+
+func getEmployeesHtml(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		tpl, err := template.ParseFiles("template.html")
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		tpl.Execute(w, employees)
+		return
+	}
+
+	http.Error(w, "Invalid method", http.StatusBadRequest)
+}
---
+++ 24_webservice/go.mod
@@ -0,0 +1,3 @@
+module 24_webservice
+
+go 1.22.0
